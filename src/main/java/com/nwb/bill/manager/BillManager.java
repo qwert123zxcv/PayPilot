b8ed6e26194db93e0e70ac3ae875b0ebb397447b
Harshit Bhatt <bhatt.harshit8@gmai.com>
2024-08-13 23:01:19 +0530

    revised code billmanager

--- src/main/java/com/nwb/bill/manager/BillManager.java
+++ src/main/java/com/nwb/bill/manager/BillManager.java
@@ -39,47 +39,6 @@ public class BillManager {
 		return overdueBills;
 	}
 	
-	//If bill category and bill name is selected
-	public List<Bill> getOverdueBills(List<Bill> bills, String bill_category, String bill_name) {
-		//Must add some code here
-		List<Bill> overdueBills=new ArrayList<>();
-
-		for(int i=0;i<bills.size();i++) {
-			if(bills.get(i).getBillCategory().trim().equals(bill_category)
-					&& bills.get(i).getBillName().trim().equals(bill_name)
-					&& bills.get(i).getOverdueDays()>0) {
-				overdueBills.add(bills.get(i));
-				break;
-			}
-		}
-		return overdueBills;
-	}
-	
-	//For filter BillCategory in UI
-	public List<Bill> getOverdueBills(List<Bill> bills, String bill_category){
-		List<Bill> overdueBills=new ArrayList<>();
-		for(int i=0;i<bills.size();i++) {
-			if(bills.get(i).getBillCategory().trim().equals(bill_category)) {
-				overdueBills.add(bills.get(i));
-			}
-		}
-		return overdueBills;
-	}
-	
-	//For filter Date Range in UI
-	public List<Bill> getOverdueBills(List<Bill> bills, Date start_date, Date end_date){
-		List<Bill> overdueBills=new ArrayList<>();
-		for(int i=0;i<bills.size();i++) {
-			Bill curr_bill=bills.get(i);
-			Date bill_date=curr_bill.getDueDate();
-			
-			if(start_date.compareTo(bill_date)<=0 && 
-					end_date.compareTo(bill_date)>=0) {
-				overdueBills.add(curr_bill);
-			}
-		}
-		return overdueBills;
-	}
     
     public List<Bill> getBillsOverview(String category, Date fromDate, Date toDate, String status) {
         List<Bill> filteredBills = new ArrayList<>();
@@ -127,5 +86,47 @@ public class BillManager {
 
         return getBillsOverview(category, fromDate, toDate, status);
     }
+    
+//	//If bill category and bill name is selected
+//	public List<Bill> getOverdueBills(List<Bill> bills, String bill_category, String bill_name) {
+//		//Must add some code here
+//		List<Bill> overdueBills=new ArrayList<>();
+//
+//		for(int i=0;i<bills.size();i++) {
+//			if(bills.get(i).getBillCategory().trim().equals(bill_category)
+//					&& bills.get(i).getBillName().trim().equals(bill_name)
+//					&& bills.get(i).getOverdueDays()>0) {
+//				overdueBills.add(bills.get(i));
+//				break;
+//			}
+//		}
+//		return overdueBills;
+//	}
+//	
+//	//For filter BillCategory in UI
+//	public List<Bill> getOverdueBills(List<Bill> bills, String bill_category){
+//		List<Bill> overdueBills=new ArrayList<>();
+//		for(int i=0;i<bills.size();i++) {
+//			if(bills.get(i).getBillCategory().trim().equals(bill_category)) {
+//				overdueBills.add(bills.get(i));
+//			}
+//		}
+//		return overdueBills;
+//	}
+//	
+//	//For filter Date Range in UI
+//	public List<Bill> getOverdueBills(List<Bill> bills, Date start_date, Date end_date){
+//		List<Bill> overdueBills=new ArrayList<>();
+//		for(int i=0;i<bills.size();i++) {
+//			Bill curr_bill=bills.get(i);
+//			Date bill_date=curr_bill.getDueDate();
+//			
+//			if(start_date.compareTo(bill_date)<=0 && 
+//					end_date.compareTo(bill_date)>=0) {
+//				overdueBills.add(curr_bill);
+//			}
+//		}
+//		return overdueBills;
+//	}
 
 }
