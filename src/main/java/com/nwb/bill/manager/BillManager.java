0fbbe821488c9173a7bf4641cdadd9669a8a65c1
Harshit Bhatt <bhatt.harshit8@gmai.com>
2024-08-14 23:25:56 +0530

    making changes

--- src/main/java/com/nwb/bill/manager/BillManager.java
+++ src/main/java/com/nwb/bill/manager/BillManager.java
@@ -1,14 +1,9 @@
 
 package com.nwb.bill.manager;
 import com.nwb.bill.model.Bill;
-
-
-import java.io.File;
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 
 public class BillManager {
@@ -28,7 +23,7 @@ public class BillManager {
 	}
 	
 	//Get all overdue bills
-	public List<Bill> getOverdueBills(List<Bill> bills) {
+	public List<Bill> getOverdueBills() {
 		//Must add some code here
 		List<Bill> overdueBills=new ArrayList<>();
 		for(int i=0;i<bills.size();i++) {
@@ -44,7 +39,11 @@ public class BillManager {
         List<Bill> filteredBills = new ArrayList<>();
         for (Bill bill : bills) {
 
-            boolean matchesCategory = bill.getBillCategory().equalsIgnoreCase(category);
+	    boolean matchesCategory = true;
+            if (!category.equalsIgnoreCase("all")) {
+            matchesCategory = bill.getBillCategory().equalsIgnoreCase(category);
+            }
+		
             boolean matchesFromDate = !bill.getDueDate().before(fromDate);
             boolean matchesToDate = !bill.getDueDate().after(toDate);
             boolean matchesStatus = bill.getPaymentStatus().equalsIgnoreCase(status);
@@ -56,36 +55,6 @@ public class BillManager {
         return filteredBills;
     }
 
-    public List<Bill> getFilteredBillsOverview() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\nView Bills Overview:");
-        System.out.print("Bill Category (All, Debt Payments, House Rent, etc.): ");
-        String category = scanner.nextLine();
-
-        System.out.print("Bill Date From (dd-MM-yyyy): ");
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-		Date fromDate = null;
-		try {
-            fromDate  = dateFormat.parse(scanner.nextLine());
-        } catch (ParseException e) {
-            System.out.println("Invalid date format. Please try again.");
-            return Collections.emptyList();
-        }
-
-        System.out.print("Bill Date To (dd-MM-yyyy): ");
-        Date toDate = null;
-        try {
-            toDate = dateFormat.parse(scanner.nextLine());
-        } catch (ParseException e) {
-            System.out.println("Invalid date format. Please try again.");
-            return Collections.emptyList();
-        }
-
-        System.out.print("Bill Status (Upcoming/Pending/Paid): ");
-        String status = scanner.nextLine();
-
-        return getBillsOverview(category, fromDate, toDate, status);
-    }
     
 //	//If bill category and bill name is selected
 //	public List<Bill> getOverdueBills(List<Bill> bills, String bill_category, String bill_name) {
